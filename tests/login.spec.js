79163155a128b5786344cfaad3e4ff129cca77fa
LoreSD <43736136+LoreSD@users.noreply.github.com>
2023-12-12 17:03:13 -0500

    Update login.spec.js

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -2,9 +2,7 @@ const { Builder, By } = require("selenium-webdriver");
 
 describe('Login', () => {
     it("Login with standard_user credentials", async() => {
-        let driver = new Builder().forBrowser('chrome').build();
-        await driver.add_argument("--disable-extensions") 
-        await driver.add_argument("--disable-dev-shm-using") 
+        let driver = new Builder().forBrowser('chrome').build()
         await driver.get('https://www.saucedemo.com/')
         await driver.findElement(By.id('user-name')).sendKeys('standard_user')
         await driver.findElement(By.css('#password')).sendKeys('secret_sauce')
