f207c968515e1fb254e1f5b4f2fbee48135115ff
LoreSD <43736136+LoreSD@users.noreply.github.com>
2023-12-12 20:52:53 -0500

    Update login.spec.js

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,13 +1,16 @@
-const { Builder, By } = require("selenium-webdriver");
+const {Builder, Browser, By, Key, until} = require('selenium-webdriver');
 
-describe('Login', () => {
-    it("Login with standard_user credentials", async() => {
-        let driver = new Builder().forBrowser('chrome').build()
-        await driver.get('https://www.saucedemo.com/')
-        await driver.findElement(By.id('user-name')).sendKeys('standard_user')
-        await driver.findElement(By.css('#password')).sendKeys('secret_sauce')
-        await driver.findElement(By.css('[data-test="login-button"]')).click()
-        await driver.sleep(5000)
-        await driver.quit();
-    });
-});
+(async function example() {
+  let driver = await new Builder().forBrowser(Browser.FIREFOX).build();
+  try {
+    await driver.get('https://www.saucedemo.com/');
+    await driver.findElement(By.id('user-name')).sendKeys('standard_user', Key.RETURN);
+    await driver.findElement(By.css('#password')).sendKeys('secret_sauce', Key.RETURN);
+    await driver.findElement(By.css('[data-test="login-button"]')).click()
+    await driver.wait(until.titleIs('webdriver - Google Search'), 5000);
+  } finally {
+    await driver.quit();
+  }
+})();
+
+
